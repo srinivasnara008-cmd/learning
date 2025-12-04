ea8cd6e923bc2d1d7442e53f075a607be7b8cd2e
srinivasnara008-cmd <srinivasnara008@gmail.com>
2025-12-04 03:06:01 -0800

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--username", required=True)
 parser.add_argument("--targetIndex", default="0")
+parser.add_argument("--targetIndex_2", default="0")
 parser.add_argument("--debug", default="false")
 print("working fine")
 
@@ -10,4 +11,5 @@ args = parser.parse_args()
 
 print(f"Username     : {args.username}")
 print(f"Target Index : {args.targetIndex}")
+print(f"Target Index 2 : {args.targetIndex_2}")
 print(f"Debug Mode   : {args.debug}")
